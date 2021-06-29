75af95788f1a6abe707c2a1cf8966c012078223a
Rajita Roy <68264150+rajitaroy@users.noreply.github.com>
2021-06-29 17:22:03 +0530

    Update fetch_events.php

--- fetch_events.php
+++ fetch_events.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 require_once('db_connection.php');
-$conn = mysqli_connect("localhost","root","","Event Portal");
+//$conn = mysqli_connect("localhost","root","","Event Portal");
+$conn = mysqli_connect("remotemysql.com","F6wy4ESYJR","ZUI1AztbHy","F6wy4ESYJR");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
