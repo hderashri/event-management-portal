9ef8dd80d7b8068e29d772cd333b9dc6d3d65383
Rajita Roy <68264150+rajitaroy@users.noreply.github.com>
2021-06-29 17:59:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 require_once('db_connection.php');
-//$conn = mysqli_connect("localhost","root","","Event Portal");
-$conn = mysqli_connect("remotemysql.com","F6wy4ESYJR","ZUI1AztbHy","F6wy4ESYJR");
+$conn = mysqli_connect("localhost","root","","Event Portal");
+//$conn = mysqli_connect("remotemysql.com","F6wy4ESYJR","ZUI1AztbHy","F6wy4ESYJR");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
