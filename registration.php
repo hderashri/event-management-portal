f0f50ce4217e7d60258d30867221d781ff9dc300
Rajita Roy <68264150+rajitaroy@users.noreply.github.com>
2021-07-01 10:52:23 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -4,8 +4,8 @@ Here, we write code for registration.
 <?php
 require_once('db_connection.php');
 
-$conn = mysqli_connect("localhost","root","","Event Portal");
-//$conn = mysqli_connect("remotemysql.com","F6wy4ESYJR","ZUI1AztbHy","F6wy4ESYJR");
+//$conn = mysqli_connect("localhost","root","","Event Portal");
+$conn = mysqli_connect("remotemysql.com","F6wy4ESYJR","ZUI1AztbHy","F6wy4ESYJR");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -18,7 +18,7 @@ $username = trim($_POST['email_field']);
 $pwd = trim($_POST['password_field']);
 $password = MD5($pwd);
 
-$sql = "INSERT INTO Users (Name,Username,Password) VALUES ('$name','$username','$password')";
+$sql = "INSERT INTO users (Name,Username,Password) VALUES ('$name','$username','$password')";
 $result = mysqli_query($conn, $sql);
 if($result)
 {
